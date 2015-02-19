a66cc3b381d1e135a1ef3fd2e302bfcc2faa5ad0
joejean <joeclef@hotmail.com>
2015-02-20 00:39:14 +0400

    Added node app

---
+++ test/test.js
@@ -0,0 +1,8 @@
+var request = require("supertest");
+var app = require("../app.js");
+
+describe("GET /", function(){
+	it("respond with Hello World! Testing Jenkins.", function(done){
+		request(app).get("/").expect('Hello World! Testing Jenkins.', done);
+	});
+});
